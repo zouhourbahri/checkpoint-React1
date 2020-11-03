0ee76b9e37cb6891f4a356abcb8f1813575c1341
zouhour <zouhour.bahri@yahoo.com>
2020-11-03 21:56:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import {Form,Button} from 'react-bootstrap';
+import {Navbar} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+  <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">
+      <img
+        alt=""
+        src="https://res.cloudinary.com/practicaldev/image/fetch/s--YvgPKF4Z--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/5crsydn14wmxeub5b2qr.jpg"
+        width="80"
+        height="50"
+        className="d-inline-block align-top"
+      />{' '}
+      <span className="navigation">React Bootstrap</span>
+    </Navbar.Brand>
+  </Navbar>
+</>
+     <Form className="position">
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+  <Button variant="primary" type="submit" className="cancelation">
+    Cancel
+  </Button>
+</Form>
+<footer className="foot">
+  <span className="copy">
+    copy right 2020 
+  </span>
+</footer>
     </div>
   );
 }
